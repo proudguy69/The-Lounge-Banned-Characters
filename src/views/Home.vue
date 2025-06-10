a1809522055fae1e593788009ff4ef589f2e4454
ProudGuy69 <idioticbatteringram@gmail.com>
2025-06-10 19:59:32 -0400

    continued on feature for category, needs to be entirely redone

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,14 +4,36 @@
 
         <UAccordion type="multiple" :items="categories" class="px-4">
 
+            <!--
             <template #genshin="{ item }">
                 <div class="mx-4 flex gap-2 justify-center flex-wrap">
                     <CharacterPreview v-for="char in genshin" :name="char.name" :ban="char.ban" :primary_image="char.primary_image" />
                 </div>
                 
+            </template>-->
+
+
+            <template #Genshin_Impact="{ item }">
+                <div class="mx-4 flex gap-2 justify-center flex-wrap">
+                    <CharacterPreview v-for="char in category_data.Genshin_Impact" :name="char.name" :ban="char.ban" :primary_image="char.primary_image" />
+                </div>
             </template>
 
+            <template #Honkai_Star_Rail="{ item }">
+                <div class="mx-4 flex gap-2 justify-center flex-wrap">
+                    <CharacterPreview v-for="char in category_data.Honkai_Star_Rail" :name="char.name" :ban="char.ban" :primary_image="char.primary_image" />
+                </div>
+            </template>
+
+            <template #Spy_x_Family="{ item }">
+                <div class="mx-4 flex gap-2 justify-center flex-wrap">
+                    <CharacterPreview v-for="char in category_data.Spy_x_Family" :name="char.name" :ban="char.ban" :primary_image="char.primary_image" />
+                </div>
+            </template>
+
+
             <template #uncatagorized="{ item }">
+                test!
                 <div class="mx-4 flex gap-2 justify-center flex-wrap">
                     <CharacterPreview v-for="char in uncatagorized" :name="char.name" :ban="char.ban" :primary_image="char.primary_image" />
                 </div>
@@ -34,18 +56,17 @@ import CharacterPreview from '@/components/CharacterPreview.vue'
 
 const categories = ref([
     {
-        label: "Genshin Impact",
-        slot: 'genshin'
-    },
-    {
         label: "Uncatagorized",
         slot: 'uncatagorized'
     },
 ])
 
+const category_data = ref({
+    uncatagorized: []
+})
+
 
 const characters = []
-const genshin = ref([])
 const uncatagorized = ref([])
 
 async function getCharacters() {
@@ -56,15 +77,29 @@ async function getCharacters() {
 
     for (let i=0; i < characters.length; i++) {
         const char = characters[i]
-        if (char.category == "Genshin Impact") {
-            genshin.value.push(char)
-        } else {
-            uncatagorized.value.push(char)
-        }
+        category_data.value[char.category.replace(/ /g, '_')].push(char)
     }
+
+    console.log(category_data)
+}
+
+async function getCategories() {
+    const response = await fetch('/api/categories')
+    const data = await response.json()
+    for (let i=0; i < data.categories.length; i++) {
+        categories.value.push({
+            label: data.categories[i],
+            slot: data.categories[i].replace(/ /g, '_')
+        })
+        category_data.value[data.categories[i].replace(/ /g, '_')] = []
+    }
+
+    console.log(categories.value)
+    console.log(category_data.value)
 }
 
 onMounted(async () => {
+    await getCategories()
     await getCharacters()
 })
 
